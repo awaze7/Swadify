99137042d810ea56fd66842494e5914316a1a80c
awaze7 <awazeshaikh7@gmail.com>
2024-01-30 18:26:46 +0530

    top rated restaurants button added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+
+    //state variable - Super powerful variable
+    const [ListOfRestaurants, setListOfRestaurants] = useState(resList);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button 
+                className="filter-btn" 
+                onClick={() => {
+                    filteredList = ListOfRestaurants.filter(
+                        (res) => res.data.avgRating > 4
+                    );
+                    setListOfRestaurants(filteredList);
+                }}
+            >
+                Top Rated Restaurants
+            </button>
+            </div>
             <div className="res-container">
                 {
-                  resList.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} /> )
+                  ListOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} /> )
                 }
             </div>
         </div>
