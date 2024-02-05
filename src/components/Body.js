0b318cd45747b59f9a87f0f83c7b3ad087d05116
awaze7 <awazeshaikh7@gmail.com>
2024-02-05 20:47:02 +0530

    added the swiggy api to fetch the restaurant data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState,useEffect } from "react";
 
 const Body = () => {
 
     //state variable - Super powerful variable
-    const [ListOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&page_type=DESKTOP_WEB_LISTING");
+        
+        const json = await data.json();
+
+        // console.log(json); //json.data.cards[4].data.data.cards
+
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants[0]);
+        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
 
     return (
         <div className="body">
@@ -14,7 +29,7 @@ const Body = () => {
                 className="filter-btn" 
                 onClick={() => {
                     filteredList = ListOfRestaurants.filter(
-                        (res) => res.data.avgRating > 4
+                        (res) => res.info.avgRating > 4.2
                     );
                     setListOfRestaurants(filteredList);
                 }}
@@ -24,7 +39,7 @@ const Body = () => {
             </div>
             <div className="res-container">
                 {
-                  ListOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} /> )
+                  ListOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant} /> )
                 }
             </div>
         </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,14 +4,13 @@ const RestaurantCard = (props) => {
     const { resData } = props;
 
     const { 
+      name,
       cloudinaryImageId, 
-      name, 
       cuisines, 
       avgRating, 
-      costForTwo, 
-      deliveryTime 
-    } = resData?.data;
-
+      costForTwo,
+    } = resData?.info ?? {};
+    const deliveryTime = resData?.info?.sla?.deliveryTime;
     return (
         <div className="res-card" style= {{backgroundColor: "#f0f0f0"}}>
             <img 
@@ -20,9 +19,10 @@ const RestaurantCard = (props) => {
             alt="rest-logo"
             />
             <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
+            <h4>{cuisines ? cuisines.join(", ") : ''}</h4>
             <h4>{avgRating} stars</h4>
-            <h4>₹{costForTwo/100} FOR TWO</h4>
+            {/* <h4>₹{costForTwo/100} FOR TWO</h4> */}
+            <h4>{costForTwo}</h4>
             <h4>{deliveryTime} minutes</h4>
         </div>
     )
