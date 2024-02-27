f43ecc6c6147777bef32859fa45bb3807fb411a6
awaze7 <awazeshaikh7@gmail.com>
2024-02-27 22:27:13 +0530

    Secure Firebase configuration with environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDc-B-2Dfcc59a-odkL0HWPv_-9xjpSMPo",
-  authDomain: "swadify-b8a22.firebaseapp.com",
-  projectId: "swadify-b8a22",
-  storageBucket: "swadify-b8a22.appspot.com",
-  messagingSenderId: "966964999731",
-  appId: "1:966964999731:web:df1d242ada45a1596a9546"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +17,3 @@ const auth = getAuth(app);
 
 export { db, auth };
 export default app;
-
